b9af310e24b45df231a1422fe6e19c42c51e4312
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2021-09-20 14:30:04 +0400

    Update sv_alium_bans.lua

--- lua/autorun/server/sv_alium_bans.lua
+++ lua/autorun/server/sv_alium_bans.lua
@@ -12,11 +12,21 @@ if game.SinglePlayer() then return end -- В сингл плеере оно на
 AliumBanList = AliumBanList or {}
 AliumBanList["Bans"] = AliumBanList["Bans"] or {}
 
+local HTTP = HTTP
+local MsgC = MsgC
+local toSteamID = util.SteamIDFrom64
+local toSteamID64 = util.SteamIDTo64
+local stSub = string.sub
+local stFind = string.find
+local stGsub = string.gsub
+local stGmatch = string.gmatch
+
 /*
     Configuration
 */
 AliumBanList["cfg"] = {
     ["banListURL"] = "https://raw.githubusercontent.com/Pika-Software/TheAliumBanList/main/banned_user.cfg",
+    ["printDisconnect"] = true,
     ["disconnectMessage"] = [[
 -------===== [ The Alium ] =====-------
 
@@ -24,11 +34,17 @@ AliumBanList["cfg"] = {
 Дополнительная информация в группе Steam:
 
 https://steamcommunity.com/groups/thealium
-]]
+]],
+    ["tag"] = "Pika Software",
+    ["colors"] = {
+        ["tagColor"] = Color(0, 103, 221),
+        ["msgColor"] = Color(224,182,42),
+        ["nickColor"] = Color(18, 184, 206),
+    }
 }
 
 function AliumBanList:Log(...)
-    MsgC(Color(0, 103, 221), "[Pika Software] ", Color(224,182,42), ..., "\n")
+    MsgC(AliumBanList["cfg"]["colors"]["tagColor"], "["..AliumBanList["cfg"]["tag"].."] ", AliumBanList["cfg"]["colors"]["msgColor"], ..., "\n")
 end
 
 function AliumBanList:Get()
@@ -42,21 +58,20 @@ function AliumBanList:Get()
                 ["accept-language"] = "en",
             },
 
-            ["success"] = function(code, body, headers)
-                if code == 200 then
+            ["success"] = function(code, body)
+                if code == 200 and body != "" then
                     AliumBanList["Bans"] = {}
-
-                    for line in body:gmatch("(.-)\n") do
-                        line = line:gsub("\r","")
-                        local st, ed = string.find(line, "banid 0 ", 0)
-                        if ed != nil then
-                            AliumBanList["Bans"][util.SteamIDTo64(string.sub(line, ed+1, #line))] = true
+                    for line in stGmatch(body,"(.-)\n") do
+                        line = stGsub(line, "\r","")
+                        local st = stFind(line, "STEAM_0:", 0)
+                        if st != nil then
+                            AliumBanList["Bans"][toSteamID64(stSub(line, st, #line))] = true
                         end
                     end
                 end
             end,
         }) != nil then
-            self:Log("Списко банов 'The Alium' успешно получен!")
+            self:Log("Списко банов от сообщества 'The Alium' успешно получен!")
         else
             self:Log("Возникла ошибка при получении списка банов!")
         end
@@ -69,20 +84,26 @@ hook.Add("InitPostEntity", "AliumBanList:GetList", function()
     AliumBanList:Get()
 end)
 
+hook.Add("CheckPassword", "AliumBanList:CheckList", function(steamid64, ip, svPass, clPass, nick)
+    if (AliumBanList["Bans"][steamid64] == true) then
+        if (AliumBanList["cfg"]["printDisconnect"] == true) then
+            MsgC(AliumBanList["cfg"]["colors"]["tagColor"], "["..AliumBanList["cfg"]["tag"].."] ", AliumBanList["cfg"]["colors"]["msgColor"], "Игрок, ", AliumBanList["cfg"]["colors"]["nickColor"], nick, " ("..toSteamID(steamid64)..")", ", был заблокирован при попытке зайти на сервер!", "\n")
+        end
+
+        return false, AliumBanList["cfg"]["disconnectMessage"]
+    end
+end)
+
 concommand.Add("alium_bans_update", function(ply)
     if IsValid(ply) then
         if ply:IsSuperAdmin() or ply:IsListenServerHost() then
             AliumBanList:Get()
         else
-            ply:SendLua('MsgC(Color(0, 103, 221), "[Pika Software] ", Color(224,182,42), "У вас недостаточно прав для данного действия!", "\n")')
+            local col1 = AliumBanList["cfg"]["colors"]["tagColor"]
+            local col2 = AliumBanList["cfg"]["colors"]["msgColor"]
+            ply:SendLua('MsgC(Color('..col["r"]..', '..col["g"]..', '..col["b"]..'), "['..AliumBanList["cfg"]["tag"]..'] ", Color('..col2["r"]..', '..col2["g"]..', '..col2["b"]..'), "У вас недостаточно прав для выполнения данного действия!", "\n")')
         end
     else
         AliumBanList:Get()
-    end
-end)
-
-hook.Add("CheckPassword", "AliumBanList:CheckList", function(steamid64, ip, svPass, clPass, nick)
-    if (AliumBanList["Bans"][steamid64] == true) then
-        return false, AliumBanList["cfg"]["disconnectMessage"]
     end
 end)
