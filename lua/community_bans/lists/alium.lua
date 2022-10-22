f65445df2cae703e0816f2465361bbfcc7ba649b
PrikolMen <olegis019@gmail.com>
2022-10-22 04:28:49 +0400

    Community Ban Lists - Core

---
+++ lua/community_bans/lists/alium.lua
@@ -0,0 +1,14 @@
+Name = 'The Alium'
+Source = 'https://raw.githubusercontent.com/Pika-Software/gmod_community_bans/main/banned_user.cfg'
+Silent = false
+Reason = [[
+    Ваш SteamID не проходит по системе
+    блокировок сателлитов или(и) вы имеете
+    особое отношение к Персонам Нон Грата
+    The Alium.
+
+    Вы можете обжаловать решение блокировки
+    в комментариях модератора Erick's Maid.
+
+    https://steamcommunity.com/groups/thealium
+]]
