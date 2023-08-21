03f7483f4999b40b8a5ab5c1b92f87a061657daa
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2023-08-21 18:37:21 +0400

    Updated repository name

--- lua/community_bans/lists/alium.lua
+++ lua/community_bans/lists/alium.lua
@@ -1,5 +1,5 @@
 Name = 'The Alium'
-Source = 'https://raw.githubusercontent.com/Pika-Software/gmod_alium_bans/main/banned_user.cfg'
+Source = 'https://raw.githubusercontent.com/Pika-Software/alium-ban-list/main/banned_user.cfg'
 Message = [[
     Ваш SteamID не проходит по системе
     блокировок сателлитов или(и) вы имеете
